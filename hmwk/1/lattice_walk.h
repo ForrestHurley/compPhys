725afb8d1eca86b4b5574ae972b9cec83525b6c8
ForrestHurley <forrestelihurley0@gmail.com>
2018-09-09 23:24:51 -0400

    Movement towards completely refactoring random walk

--- hmwk/1/lattice_walk.h
+++ hmwk/1/lattice_walk.h
@@ -5,39 +5,71 @@
 #include <unordered_map>
 #include <iostream>
 
-template <int N>
-class lattice_walk
+template <class point_type, int N>
+class random_walk
 {
 public:
-  using point = std::array<int, N>
+  using point = std::array<point_type, N>
 
 private:
   std::vector<point> location_list;
 
 protected:
-  point generate_nth_step(int index);
-  std::array<int, N * 2> generate_all_steps();
-  point generate_random_step();
+  virtual point generate_random_step();
 
-  virtual void push_point(point in_point);
-  virtual point pop_point();
-  
   virtual bool step_forwards(int steps);
   virtual void step_backwards(int steps);
 
 public:
-  lattice_walk();
-
-  void make_walk(int steps);
+  virtual void push_point(point in_point);
+  virtual point pop_point();
+  
 
   int get_length();
 
   std::vector<point> get_location_list();
   point get_single_location(int index);
+
+  void make_walk(int steps);
 }
 
 template <int N>
-lattice_walk<N>::lattice_walk();
+class lattice_walk : public random_walk<int, N>
+{
+protected:
+  point generate_nth_step(int index);
+  std::array<int, N * 2> generate_all_steps();
+  point generate_random_step() override;
+}
+
+template <class point_type, int N>
+virtual void random_walk<point_type, N>::push_point(random_walk<class, N>::point in_point);
+
+template <class point_type, int N>
+virtual random_walk<point_type, N>::point random_walk<point_type, N>::pop_point();
+
+template <class point_type, int N>
+virtual bool random_walk<point_type, N>::step_forwards(int steps);
+
+template <class point_type, int N>
+virtual void random_walk<point_type, N>::step_backwards(int steps);
+
+template <class point_type, int N>
+void random_walk<point_type, N>::make_walk(int steps);
+
+template <class point_type, int N>
+int random_walk<point_type, N>::get_length();
+
+template <class point_type, int N>
+std::vector<random_walk<point_type, N>::point> random_walk<point_type, N>::get_location_list();
+
+template <class point_type, int N>
+random_walk<point_type, N>::point random_walk<point_type, N>::get_single_location(int index);
+
+template <int N>
+random_walk<point_type, N>::point random_walk<point_type, N>::generate_random_step();
+
+////////////////////////////////////////////
 
 template <int N>
 lattice_walk<N>::point lattice_walk<N>::generate_nth_step(int index);
@@ -47,29 +79,5 @@ std::array<int, N * 2> lattice_walk<N>::generate_all_steps();
 
 template <int N>
 lattice_walk<N>::point lattice_walk<N>::generate_random_step();
-
-template <int N>
-virtual void point_lattice<N>::push_point(point_lattice<N>::point in_point);
-
-template <int N>
-virtual point_lattice<N>::point point_lattice<N>::pop_point();
-
-template <int N>
-virtual bool point_lattice<N>::step_forwards(int steps);
-
-template <int N>
-virtual void point_lattice<N>::step_backwards(int steps);
-
-template <int N>
-void point_lattice<N>::make_walk(int steps);
-
-template <int N>
-int point_lattice<N>::get_length();
-
-template <int N>
-std::vector<point_lattice<N>::point> point_lattice<N>::get_location_list();
-
-template <int N>
-point_lattice<N>::point point_lattice<N>::get_single_location(int index);
 
 #endif
